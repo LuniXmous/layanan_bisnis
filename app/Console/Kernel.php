ff5097ae053035493d947d2189b77a9d21827fea
Fayyaz <abdullahnaufal16@gmail.com>
2025-09-20 23:21:08 +0700

    Cc

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     }
 
     protected $commands = [
-        \App\Console\Commands\ReminderPengajuanAktor::class,
+        \App\Console\commands\ReminderPengajuanAktor::class,
     ];
 
     protected function schedule(Schedule $schedule)
